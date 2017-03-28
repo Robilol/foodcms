b0fa2a03c2dc5cd320a08512abdee3e27cf9a594
Highlife972 <a.cheminat@hotmail.com>
2017-03-28 19:35:35 +0200

    Style layout

--- views/frontend.view.php
+++ views/frontend.view.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Titre de la page</title>
-        <meta name="description" content="description de la page">
-        <link rel="stylesheet" href="assets/css/style.css">
-    </head>
-    <body>
-
-        <?php
-            include $this->view.".view.php";
-         ?>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <title>Ma page food CMS</title>
+  <meta name="description" content="description de la page">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <div class="menu">
+    <ul>
+      <li><img class="logo" src="/assets/logo.png"></li>
+      <div class="rightli">
+        <li><a href="/">S'INSCRIRE</a></li>
+        <li><a href="/">S'IDENTIFIER</a></li>
+      </div>
+    </ul>
+  </div>
+  <?php
+  include $this->view.".view.php";
+  ?>
+</body>
 </html>
