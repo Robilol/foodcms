47ecefb9715517f336634f80ff56d449fd68eeb3
Douae EL OMARI <elomaridouae@gmail.com>
2017-07-05 15:03:38 +0200

    Gestion des listes back

--- controllers/Admin/ArticleController.class.php
+++ controllers/Admin/ArticleController.class.php
@@ -5,6 +5,9 @@ class ArticleController {
 
     public function indexAction(){
       $v = new View("admin/articleCreate","backend");
+        $article = new Article(-1);
+        $allArticles = $article->getAll();
+        $v->assign("allArticles", $allArticles);
     }
 
     public function listAction(){
