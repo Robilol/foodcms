132faefcb0ba37a3e3d93ea2e792eba77d2820c6
Douae EL OMARI <elomaridouae@gmail.com>
2017-07-24 13:28:53 +0200

    Add comment sur article

--- controllers/CommentController.class.php
+++ controllers/CommentController.class.php
@@ -3,16 +3,18 @@
 class CommentController
 {
 
-    public function addAction() {
-        
+    public function createCommentAction() {
+        $data = $_POST;
+        var_dump($data);
+      //$comment = new Commet(-1, $data['title'], $data['category'], $data['active']);
+    //  $comment->save();
     }
 
     public function editAction() {
-        
+
     }
 
     public function deleteAction() {
-        
+
     }
 }
-
